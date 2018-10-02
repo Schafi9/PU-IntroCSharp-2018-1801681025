6c3f832d8fd7a44b3866b9838ee735a309155572
Schafi9 <kristiqnged@gmail.com>
2018-10-02 16:03:53 +0300

    included homework t1 & t2

---
+++ PUIntroHomeworks/AgeAfter10Years/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AgeAfter10Years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 10;
+            int after10Years = myAge + 10;
+
+            Console.WriteLine("My age after 10 years =>!" + after10Years);
+        }
+    }
+}
