1258c845d6535b4d39eb22bfb07d63fa12da39c5
Schafi9 <kristiqnged@gmail.com>
2018-10-09 16:07:47 +0300

    travel to sun

---
+++ PUIntroHomeworks/Travel2TheSunWithSoundSpeedInWater/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Travel2TheSunWithSoundSpeedInWater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distance2SunFromEarth = 149600000;
+            short speedOfSoundIn20DegreeWater = 1481;
+
+            double timeNeeded = (double)distance2SunFromEarth / speedOfSoundIn20DegreeWater;
+            Console.WriteLine($"Time needed:{timeNeeded}");
+        }
+    }
+}
